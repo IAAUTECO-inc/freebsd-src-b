maimere/MyOwnProprietaryFreeBSD

commit 5cafd6213f145bbe2a70d55eb90a3d894e66115b

exterr_set: sync the definition with the header declaration
This unbreaks buildkernel with TARGET=armv7 (32-bit arm). More work may be required
in order to unbreak `exterr_set` with 32-bit kernels.

Fixes:	844009378da9 ("exterr: allow exterr to fit pointers on CHERI targets")

sys/kern/sys_generic.c
@@ -2353,8 +2353,8 @@ kern_exterrctl(struct thread *td, u_int op, u_int flags, void *ptr)
 }
 
 int
-exterr_set(int eerror, int category, const char *mmsg, uintptr_t pp1,
-    uintptr_t pp2, int line)
+exterr_set(int eerror, int category, const char *mmsg, uintptr64_t pp1,
+    uintptr64_t pp2, int line)
 {
 	struct thread *td;
 

IAAUTECO-inc/freebsd-src-b

commit 9da8f0e0b4bd4fe4e60ddd2baf9294b28d155cc4

kern: fix compilation
uintptr64_t -> uint64ptr_t

Fixes:  5cafd6213f145 (exterr_set: sync the definition with the header declaration)

sys/kern/sys_generic.c
@@ -2353,8 +2353,8 @@ kern_exterrctl(struct thread *td, u_int op, u_int flags, void *ptr)
 }
 
 int
-exterr_set(int eerror, int category, const char *mmsg, uintptr64_t pp1,
-    uintptr64_t pp2, int line)
+exterr_set(int eerror, int category, const char *mmsg, uint64ptr_t pp1,
+    uint64ptr_t pp2, int line)
 {
 	struct thread *td;
 