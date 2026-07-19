IAAUTECO-inc/freebsd-src-b

commit bdc0f7678257eaa739b9c816285504470e71e3de

hwpstate_intel: Fix i386 build
Reviewed by:    olce
Fixes:  7b26353a59d6
MFC after:      3 days
Sponsored by:   The FreeBSD Foundation
Differential Revision: https://reviews.freebsd.org/D58208

sys/x86/cpufreq/hwpstate_intel.c
@@ -352,13 +352,19 @@ intel_hwpstate_hybrid_cb(void *ctx)
 {
 	uint32_t *small_cores = ctx;
 
+#ifdef __i386__
+	(void)small_cores;
+#else
 	atomic_add_32(small_cores, PCPU_GET(small_core));
+#endif
 }
 
 void
 intel_hwpstate_identify(driver_t *driver, device_t parent)
 {
+#ifndef __i386__
 	uint32_t small_cores = 0;
+#endif
 
 	if (device_find_child(parent, "hwpstate_intel", DEVICE_UNIT_ANY) != NULL)
 		return;
@@ -379,6 +385,7 @@ intel_hwpstate_identify(driver_t *driver, device_t parent)
 	if ((cpu_power_eax & CPUTPM1_HWP) == 0)
 		return;
 
+#ifndef __i386__
 	/*
 	 * On hybrid-core systems, package-level control cannot be used.
 	 * It may cause all cores to run at the E-core frequency because
@@ -389,6 +396,7 @@ intel_hwpstate_identify(driver_t *driver, device_t parent)
 	    intel_hwpstate_hybrid_cb, smp_no_rendezvous_barrier, &small_cores);
 	if (small_cores > 0 && small_cores < mp_ncores)
 		hwpstate_pkg_ctrl_enable = false;
+#endif
 
 	if (BUS_ADD_CHILD(parent, 10, "hwpstate_intel", device_get_unit(parent))
 	    == NULL)

commit 02c440e204041e92da403a64b70c2e1fdf3a4f73

hwpstate_intel: Minimize ifdef for i386 build
Reported by:    jrtc27
Fixes:  bdc0f7678257
MFC after:      3 days
Sponsored by:   The FreeBSD Foundation

sys/x86/cpufreq/hwpstate_intel.c
@@ -362,9 +362,7 @@ intel_hwpstate_hybrid_cb(void *ctx)
 void
 intel_hwpstate_identify(driver_t *driver, device_t parent)
 {
-#ifndef __i386__
 	uint32_t small_cores = 0;
-#endif
 
 	if (device_find_child(parent, "hwpstate_intel", DEVICE_UNIT_ANY) != NULL)
 		return;
@@ -385,7 +383,6 @@ intel_hwpstate_identify(driver_t *driver, device_t parent)
 	if ((cpu_power_eax & CPUTPM1_HWP) == 0)
 		return;
 
-#ifndef __i386__
 	/*
 	 * On hybrid-core systems, package-level control cannot be used.
 	 * It may cause all cores to run at the E-core frequency because
@@ -396,7 +393,6 @@ intel_hwpstate_identify(driver_t *driver, device_t parent)
 	    intel_hwpstate_hybrid_cb, smp_no_rendezvous_barrier, &small_cores);
 	if (small_cores > 0 && small_cores < mp_ncores)
 		hwpstate_pkg_ctrl_enable = false;
-#endif
 
 	if (BUS_ADD_CHILD(parent, 10, "hwpstate_intel", device_get_unit(parent))
 	    == NULL)