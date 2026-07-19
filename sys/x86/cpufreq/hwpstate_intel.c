freebsd/freebsd-src

commit 7b26353a59d66dc1bc611fd042a49b9e3bd13699

hwpstate_intel: Disable package control on hybrid CPU
In package control mode, the performance of all cores depends on the
most recent value written to the request field. If the last write comes
from an E-core, all cores are forced to align with the E-core
performance level, resulting in significant performance degradation.
Therefore, package control is disabled on hybrid-core systems.

Reviewed by:    olce
MFC after:      2 weeks
Sponsored by:   The FreeBSD Foundation
Sponsored by:   Framework Computer Inc
Differential Revision: https://reviews.freebsd.org/D57377

sys/x86/cpufreq/hwpstate_intel.c
@@ -321,9 +321,19 @@ out:
 	return (ret);
 }
 
+static void
+intel_hwpstate_hybrid_cb(void *ctx)
+{
+	uint32_t *small_cores = ctx;
+
+	atomic_add_32(small_cores, PCPU_GET(small_core));
+}
+
 void
 intel_hwpstate_identify(driver_t *driver, device_t parent)
 {
+	uint32_t small_cores = 0;
+
 	if (device_find_child(parent, "hwpstate_intel", DEVICE_UNIT_ANY) != NULL)
 		return;
 
@@ -342,6 +352,17 @@ intel_hwpstate_identify(driver_t *driver, device_t parent)
 	 */
 	if ((cpu_power_eax & CPUTPM1_HWP) == 0)
 		return;
+
+	/*
+	 * On hybrid-core systems, package-level control cannot be used.
+	 * It may cause all cores to run at the E-core frequency because
+	 * the resulting package frequency depends on the last core that
+	 * sets the frequency.
+	 */
+	smp_rendezvous_cpus(all_cpus, smp_no_rendezvous_barrier,
+	    intel_hwpstate_hybrid_cb, smp_no_rendezvous_barrier, &small_cores);
+	if (small_cores > 0 && small_cores < mp_ncores)
+		hwpstate_pkg_ctrl_enable = false;
 
 	if (BUS_ADD_CHILD(parent, 10, "hwpstate_intel", device_get_unit(parent))
 	    == NULL)

IAAUTECO-inc/freebsd-src-b

commit bdc0f7678257eaa739b9c816285504470e71e3de

hwpstate_intel: Fix i386 build
Reviewed by:    olce
Fixes:  7b26353a59d6
MFC after:      3 days
Sponsored by:   The FreeBSD Foundation
Differential Revision: https://reviews.freebsd.org/D58208

sys/x86/cpufreq/hwpstate_intel.c
@@ -352,13 +352,19 @@ intel_hwpstate_hybrid_cb(void *ctx)
 {
 	uint32_t *small_cores = ctx;
 
+#ifdef __i386__
+	(void)small_cores;
+#else
 	atomic_add_32(small_cores, PCPU_GET(small_core));
+#endif
 }
 
 void
 intel_hwpstate_identify(driver_t *driver, device_t parent)
 {
+#ifndef __i386__
 	uint32_t small_cores = 0;
+#endif
 
 	if (device_find_child(parent, "hwpstate_intel", DEVICE_UNIT_ANY) != NULL)
 		return;
@@ -379,6 +385,7 @@ intel_hwpstate_identify(driver_t *driver, device_t parent)
 	if ((cpu_power_eax & CPUTPM1_HWP) == 0)
 		return;
 
+#ifndef __i386__
 	/*
 	 * On hybrid-core systems, package-level control cannot be used.
 	 * It may cause all cores to run at the E-core frequency because
@@ -389,6 +396,7 @@ intel_hwpstate_identify(driver_t *driver, device_t parent)
 	    intel_hwpstate_hybrid_cb, smp_no_rendezvous_barrier, &small_cores);
 	if (small_cores > 0 && small_cores < mp_ncores)
 		hwpstate_pkg_ctrl_enable = false;
+#endif
 
 	if (BUS_ADD_CHILD(parent, 10, "hwpstate_intel", device_get_unit(parent))
 	    == NULL)